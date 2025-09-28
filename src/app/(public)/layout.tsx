816c7ad0736018510caec5e3108dbee3d81c0d16
Somya <m2somya@gmail.com>
2025-09-28 21:44:53 +0530

    added react node

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from "react";
 import PublicHeader from "@/components/PublicHeader";
 import Footer from "@/components/Footer";
 
-export default function PublicLayout({ children }: { children: React.ReactNode }) {
+export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-dvh flex flex-col">
       <PublicHeader />
